04760c31cf2e612574d62b5a8de3ca419bb4ecac
mynkq <refreshedmayank@gmail.com>
2026-05-23 18:51:06 +0530

    ...

--- frontend/src/app/feed/page.tsx
+++ frontend/src/app/feed/page.tsx
@@ -2,21 +2,25 @@
 import Feedcard from "../components/Feedcard";
 import axios from "axios";
 import { useState, useEffect } from "react";
- function Feed(){
-    const [feeddata, setFeeddata] = useState([]);
-    async function fetchfeeddata(){
-        const response = await axios.get("https://social-feed-app-wf5p.onrender.com/");
-        setFeeddata(response.data.posts);
-        console.log(response.data.posts)
-    
-    }
-    useEffect(() => {fetchfeeddata()}, [])
-    
 
-    return(<div>
-        {feeddata.map((feed)=>(
-            <Feedcard key={feed._id} Image={feed.Image} Caption={feed.Caption} />
-        ))}
-    </div>)
+function Feed() {
+  const [feeddata, setFeeddata] = useState<{_id: string; Image: string; Caption: string}[]>([]);
+
+  async function fetchfeeddata() {
+    const response = await axios.get("https://social-feed-app-wf5p.onrender.com/");
+    setFeeddata(response.data.posts);
+    console.log(response.data.posts);
+  }
+
+  useEffect(() => { fetchfeeddata() }, [])
+
+  return (
+    <div>
+      {feeddata.map((feed) => (
+        <Feedcard key={feed._id} Image={feed.Image} Caption={feed.Caption} />
+      ))}
+    </div>
+  )
 }
-export default Feed; 
+
+export default Feed;
